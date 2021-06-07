e01c9ef1006257436472882afe053c1ee9a4ec7a
Игорь <rm.gooogle@gmail.com>
2021-06-08 00:44:26 +0300

    reformat file GrandMasterControllerTest

--- src/test/java/com/ntiteam/task/controller/GrandMasterControllerTest.java
+++ src/test/java/com/ntiteam/task/controller/GrandMasterControllerTest.java
@@ -2,33 +2,27 @@ package com.ntiteam.task.controller;
 
 import com.ntiteam.task.dto.MasterDto;
 import com.ntiteam.task.dto.PlanetDto;
-import com.ntiteam.task.model.Master;
-import com.ntiteam.task.model.Planet;
 import com.ntiteam.task.service.WorldMasterService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -43,7 +37,7 @@ class GrandMasterControllerTest {
     MockMvc mockMvc;
 
     MasterDto masterDto = new MasterDto();
-    List <MasterDto> masterDtos = new ArrayList<>();
+    List<MasterDto> masterDtos = new ArrayList<>();
     PlanetDto planetDto = new PlanetDto();
     List<PlanetDto> planetDtos = new ArrayList<>();
 
@@ -52,15 +46,11 @@ class GrandMasterControllerTest {
         MockitoAnnotations.initMocks(this);
         planetDto.setName("TestName");
         planetDto.setId(1L);
-
         planetDtos.add(planetDto);
-
         masterDto.setId(1L);
         masterDto.setName("TestName");
         masterDto.setAge(26L);
-
         masterDtos.add(masterDto);
-
         grandMasterController = new GrandMasterController(worldMasterService);
         mockMvc = MockMvcBuilders
                 .standaloneSetup(grandMasterController)
@@ -89,7 +79,7 @@ class GrandMasterControllerTest {
 
     @Test
     void setPlanetToMaster() throws Exception {
-        Mockito.when(worldMasterService.updateMasterByPlanet(any(),any())).thenReturn(masterDto);
+        Mockito.when(worldMasterService.updateMasterByPlanet(any(), any())).thenReturn(masterDto);
         mockMvc.perform(post("/edit/master/5/5"))
                 .andExpect(status().isOk());
     }
