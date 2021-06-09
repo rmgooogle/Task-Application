6581231a96de2fc6fb4b025d2ca9da6ab9c9ae4c
Игорь <rm.gooogle@gmail.com>
2021-06-09 13:30:27 +0300

    Added description for methods

--- src/main/java/com/ntiteam/task/controller/GrandMasterController.java
+++ src/main/java/com/ntiteam/task/controller/GrandMasterController.java
@@ -4,6 +4,8 @@ import com.ntiteam.task.dto.MasterDto;
 import com.ntiteam.task.dto.SaveMasterDto;
 import com.ntiteam.task.dto.UpdateDto;
 import com.ntiteam.task.service.WorldMasterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,33 +20,39 @@ public class GrandMasterController {
         this.worldMasterService = worldMasterService;
     }
 
+    @Operation(summary = "Возвращает список повелителей")
     @GetMapping({"/get/all"})
     public ResponseEntity<List<MasterDto>> getAll() {
         return ResponseEntity.ok(worldMasterService.getAll());
     }
 
+    @Operation(summary = "Возвращает повелителей по {id}")
     @GetMapping({"/get/master/{id}"})
     public ResponseEntity<MasterDto> getMasterById(@PathVariable Long id) {
         return ResponseEntity.ok(worldMasterService.getMasterById(id));
     }
 
+    @Operation(summary = "Создает нового повелителя с параметрами {{name}, {age}}")
     @PostMapping("/add/master")
     public ResponseEntity<String> addMaster(@RequestBody SaveMasterDto dto) {
         worldMasterService.createMaster(dto);
         return ResponseEntity.ok("New Master added");
     }
 
+    @Operation(summary = "Назначает повелителю с {masterId} руководить планетой c {planetId}")
     @PostMapping("/edit/master")
     public ResponseEntity<String> setPlanetToMaster(@RequestBody UpdateDto dto) {
         worldMasterService.updateMasterByPlanet(dto);
         return ResponseEntity.ok("Upload!");
     }
 
+    @Operation(summary = "Возвращает повелителей-бездельников")
     @GetMapping("/get/masters/slacker")
     public ResponseEntity<List<MasterDto>> getSlackers() {
         return ResponseEntity.ok(worldMasterService.getSlackers());
     }
 
+    @Operation(summary = "Возвращает повелителей по старшенству, максимально 10 повелителей")
     @GetMapping("/get/masters/young")
     public ResponseEntity<List<MasterDto>> getTenYoungMasters() {
         return ResponseEntity.ok(worldMasterService.getTenYoungMasters());
--- src/main/java/com/ntiteam/task/controller/PlanetController.java
+++ src/main/java/com/ntiteam/task/controller/PlanetController.java
@@ -2,6 +2,7 @@ package com.ntiteam.task.controller;
 
 import com.ntiteam.task.dto.PlanetDto;
 import com.ntiteam.task.service.WorldMasterService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,17 +15,20 @@ public class PlanetController {
         this.worldMasterService = worldMasterService;
     }
 
+    @Operation(summary = "Возвращает планету по {id}")
     @GetMapping({"/get/planet/{id}"})
     public ResponseEntity<PlanetDto> getPlanetById(@PathVariable Long id) {
         return ResponseEntity.ok(worldMasterService.getPlanetById(id));
     }
 
+    @Operation(summary = "Создает планету с именем {name}")
     @PostMapping(value = "/add/planet/{name}")
     public ResponseEntity<String> createPlanet(@PathVariable String name) {
         worldMasterService.createPlanet(name);
         return ResponseEntity.ok("Created");
     }
 
+    @Operation(summary = "Удаляет планету по {id}")
     @DeleteMapping({"/delete/planet/{id}"})
     public ResponseEntity<String> deletePlanetById(@PathVariable Long id) {
         worldMasterService.deletePlanetById(id);
